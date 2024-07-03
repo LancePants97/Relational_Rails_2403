d8a483c6983a7bcaa164c18aa7044f0210f65551
Lance Nelson <lancen04@gmail.com>
2024-07-03 19:44:54 -0400

    updated user story 1

--- spec/features/breweries/index_spec.rb
+++ spec/features/breweries/index_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Breweries Index Page' do
-
-     # User story 1, parent index
-     it 'displays the brewery name' do
-        tlb = Brewery.create!(name: "Twin Lights", beers_on_tap: 8, pet_friendly: true)
-        kane = Brewery.create!(name: 'Kane', beers_on_tap: 15, pet_friendly: false)
+	before(:each) do 
+		@tlb = Brewery.create!(name: "Twin Lights", beers_on_tap: 8, pet_friendly: true)
+    @kane = Brewery.create!(name: 'Kane', beers_on_tap: 15, pet_friendly: false)
+	end
+  # User story 1, parent index
+  it 'displays the brewery name' do
         
-        visit "/breweries"
-        # save_and_open_page
+    visit "/breweries"
+    # save_and_open_page
 
-        expect(page).to have_content(tlb.name)
-    end
+    expect(page).to have_content(@tlb.name)
+		expect(page).to have_content(@kane.name)
+  end
 end 

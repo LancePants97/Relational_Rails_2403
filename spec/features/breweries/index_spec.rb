7956390649fad5ae351c11afd250bf9847caad20
Lance Nelson <lancen04@gmail.com>
2024-07-03 19:56:20 -0400

    updated user story 2 and made tests more robust

--- spec/features/breweries/index_spec.rb
+++ spec/features/breweries/index_spec.rb
@@ -5,13 +5,24 @@ RSpec.describe 'Breweries Index Page' do
 		@tlb = Brewery.create!(name: "Twin Lights", beers_on_tap: 8, pet_friendly: true)
     @kane = Brewery.create!(name: 'Kane', beers_on_tap: 15, pet_friendly: false)
 	end
+
   # User story 1, parent index
   it 'displays the brewery name' do
-        
     visit "/breweries"
-    # save_and_open_page
 
-    expect(page).to have_content(@tlb.name)
-		expect(page).to have_content(@kane.name)
+    within("#breweries") do
+      expect(page).to have_content(@tlb.name)
+      expect(page).to have_content(@kane.name)
+    end
+
+    within("#brewery-#{@tlb.id}") do
+      expect(page).to have_content(@tlb.name)
+      expect(page).to_not have_content(@kane.name)
+    end
+
+    within("#brewery-#{@kane.id}") do
+		  expect(page).to have_content(@kane.name)
+      expect(page).to_not have_content(@tlb.name)
+    end
   end
 end 
--- spec/features/breweries/show_spec.rb
+++ spec/features/breweries/show_spec.rb
@@ -1,36 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'the breweries show page' do
-    # User Story 2
-    it 'can show the brewerys name, beers on tap, and pet friendly status' do
-        tlb = Brewery.create!(name: 'Twin Lights', beers_on_tap: 8, pet_friendly: true)
-        kane = Brewery.create!(name: 'Kane', beers_on_tap: 15, pet_friendly: false)
-        
-        visit "/breweries/#{tlb.id}"
+	before(:each) do 
+		@tlb = Brewery.create!(name: 'Twin Lights', beers_on_tap: 8, pet_friendly: true)
+    @kane = Brewery.create!(name: 'Kane', beers_on_tap: 15, pet_friendly: false)
+	end
+  # User Story 2
+  it 'can show the brewerys name, beers on tap, and pet friendly status' do    
+    visit "/breweries/#{@tlb.id}"
 
-        # save_and_open_page
 
-        expect(page).to have_content("Twin Lights")
-        expect(page).to have_content("8")
-        expect(page).to have_content("Pet Friendly: true")
-        expect(page).to_not have_content("Kane")
-        expect(page).to_not have_content("15")
-        expect(page).to_not have_content("Pet Friendly: false")
-    end
+    expect(page).to have_content("Twin Lights")
+    expect(page).to have_content("8")
+    expect(page).to have_content("Pet Friendly: true")
 
-    it 'can show another brewerys name, beers on tap, and pet friendly status' do
-        tlb = Brewery.create!(name: 'Twin Lights', beers_on_tap: 8, pet_friendly: true)
-        kane = Brewery.create!(name: 'Kane', beers_on_tap: 15, pet_friendly: false)
-        
-        visit "/breweries/#{kane.id}"
+    expect(page).to_not have_content("Kane")
+    expect(page).to_not have_content("15")
+    expect(page).to_not have_content("Pet Friendly: false")
+  end
 
-        # save_and_open_page
+  it 'can show another brewerys name, beers on tap, and pet friendly status' do 
+    visit "/breweries/#{@kane.id}"
 
-        expect(page).to_not have_content("Twin Lights")
-        expect(page).to_not have_content("8")
-        expect(page).to_not have_content("Pet Friendly: true")
-        expect(page).to have_content("Kane")
-        expect(page).to have_content("15")
-        expect(page).to have_content("Pet Friendly: false")
-    end
+    expect(page).to_not have_content("Twin Lights")
+  	expect(page).to_not have_content("8")
+  	expect(page).to_not have_content("Pet Friendly: true")
+
+  	expect(page).to have_content("Kane")
+  	expect(page).to have_content("15")
+  	expect(page).to have_content("Pet Friendly: false")
+	end
 end

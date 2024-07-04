d79987f14d3e3f1d7d1480fb6c4ef4af9391e9a5
Lance Nelson <lancen04@gmail.com>
2024-07-03 20:17:53 -0400

    corrected user story 4

--- spec/features/beers/show_spec.rb
+++ spec/features/beers/show_spec.rb
@@ -1,48 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe 'the beers show page' do
-    # User story 4, child show
-    it 'displays the beer style' do
-        brewery = Brewery.create!(name: "Twin Lights", beers_on_tap: 8, pet_friendly: true)
-        beer = brewery.beers.create!(style_name: "Lager", abv: 5, dry_hop: false)
-        beer_2 = brewery.beers.create!(style_name: "Double Vision", abv: 6, dry_hop: true)
-        
-        visit "/beers/#{beer.id}"
-        # save_and_open_page
+  before(:each) do 
+		@tlb = Brewery.create!(name: 'Twin Lights', beers_on_tap: 8, pet_friendly: true)
+    @kane = Brewery.create!(name: 'Kane', beers_on_tap: 15, pet_friendly: false)
+	
+		@lager = @tlb.beers.create!(style_name: 'Lager', abv: 5, dry_hop: false)
+		@dv = @tlb.beers.create!(style_name: 'Double Vision', abv: 6, dry_hop: true)
+		@head_high = @kane.beers.create!(style_name: 'Head High', abv: 8, dry_hop: true)
+		@sneakbox = @kane.beers.create!(style_name: 'Sneak Box', abv: 7, dry_hop: false)
+	end
 
-        expect(page).to have_content(beer.style_name)
-        expect(page).to have_content("Lager")
-        expect(page).to have_content(beer.abv)
-        expect(page).to have_content("5")
-        expect(page).to have_content(beer.dry_hop)
-        expect(page).to have_content("Dry Hopped: false")
-        # expect(page).to have_content("Double Vision")
-        # # expect(page).to have_content("6")
-        # # expect(page).to have_content("Dry Hopped: true")
-        expect(page).to_not have_content(beer_2.style_name)
-    end
+  # User story 4, child show
+  it 'displays the beer style' do
+    visit "/beers/#{@lager.id}"
 
-    it 'displays the beer style' do
-        brewery = Brewery.create!(name: "Twin Lights", beers_on_tap: 8, pet_friendly: true)
-        beer = brewery.beers.create!(style_name: "Lager", abv: 5, dry_hop: false)
-        beer_2 = brewery.beers.create!(style_name: "Double Vision", abv: 6, dry_hop: true)
-        
-        visit "/beers/#{beer_2.id}"
-        # save_and_open_page
+    expect(page).to have_content("Lager")
+    expect(page).to have_content("ABV: 5")
+    expect(page).to have_content("Dry Hopped: false")
 
-        expect(page).to_not have_content(beer.style_name)
-        expect(page).to_not have_content("Lager")
-        expect(page).to_not have_content(beer.abv)
-        expect(page).to_not have_content("5")
-        expect(page).to_not have_content(beer.dry_hop)
-        expect(page).to_not have_content("Dry Hopped: false")
+		expect(page).to_not have_content("Double Vision")
+    expect(page).to_not have_content("ABV: 6")
+    expect(page).to_not have_content("Dry Hopped: true")
+	end
 
-        expect(page).to have_content(beer_2.style_name)
-        expect(page).to have_content("Double Vision")
-        expect(page).to have_content(beer_2.style_name)
-        expect(page).to have_content("6")
-        expect(page).to have_content("Dry Hopped: true")
-    end
+	it 'displays the beer style' do
+    visit "/beers/#{@dv.id}"
 
-    
+    expect(page).to have_content("Double Vision")
+    expect(page).to have_content("ABV: 6")
+    expect(page).to have_content("Dry Hopped: true")
+
+		expect(page).to_not have_content("Lager")
+    expect(page).to_not have_content("ABV: 5")
+    expect(page).to_not have_content("Dry Hopped: false")
+  end
+
+	it 'displays the beer style' do
+    visit "/beers/#{@head_high.id}"
+
+    expect(page).to have_content("Head High")
+    expect(page).to have_content("ABV: 8")
+    expect(page).to have_content("Dry Hopped: true")
+
+		expect(page).to_not have_content("Sneak Box")
+    expect(page).to_not have_content("ABV: 7")
+    expect(page).to_not have_content("Dry Hopped: false")
+  end
+
+	it 'displays the beer style' do
+    visit "/beers/#{@sneakbox.id}"
+
+    expect(page).to have_content("Sneak Box")
+    expect(page).to have_content("ABV: 7")
+    expect(page).to have_content("Dry Hopped: false")
+
+		expect(page).to_not have_content("Head High")
+    expect(page).to_not have_content("ABV: 8")
+    expect(page).to_not have_content("Dry Hopped: true")
+  end
 end

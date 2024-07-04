504cb9782159f58761aeab7b3d738e447ab70d10
Lance Nelson <lancen04@gmail.com>
2024-07-03 21:10:55 -0400

    completed user story 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ApplicationRecord
-    has_many :beers
+  has_many :beers
+
+	def self.order_by_recently_created
+		order(created_at: :desc)
+	end
+	
 end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Brewery do
-    it {should have_many :beers}
+  it {should have_many :beers}
+
+  describe "class methods" do 
+    it "self.order_by_recently_created" do 
+      tlb = Brewery.create!(name: "Twin Lights", beers_on_tap: 8, pet_friendly: true)
+      kane = Brewery.create!(name: "Kane", beers_on_tap: 15, pet_friendly: false)
+      new_brewery = Brewery.create!(name: "Avery Brewing", beers_on_tap: 30, pet_friendly: true)
+
+      breweries = Brewery.all
+      expect(breweries.order_by_recently_created).to eq([new_brewery, kane, tlb])
+    end
+  end
 end
+
 
